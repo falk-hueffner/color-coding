06af8dbf118c9bf4c7d09159d921e049ad0c6336
Thomas Zichner <tzi@minet.uni-jena.de>
2006-03-09 09:34:36 +0000

    Änderung an Variablen bzw. Typenamen MAP -> MAP_STR_INT PAIR -> PAIR_STR_INT MAP_ITERATOR -> MAP_STR_INT_ITER
    überall, wo knode auftauchte, steht jetzt node

--- colorcoding.cpp
+++ colorcoding.cpp
@@ -11,9 +11,9 @@ typedef vector<int> VEC_INT;
 typedef vector<VEC_INT> VEC_VEC_INT;
 typedef vector<float> VEC_FLT;
 typedef vector<VEC_FLT> VEC_VEC_FLT;
-typedef map<string, int> MAP;
-typedef MAP::iterator MAP_ITERATOR;
-typedef pair<string, int> PAIR;
+typedef map<string, int> MAP_STR_INT;
+typedef MAP_STR_INT::iterator MAP_STR_INT_ITER;
+typedef pair<string, int> PAIR_STR_INT;
 
 typedef vector<string> VEC_STR;
 
@@ -38,19 +38,19 @@ typedef MAP_FLT_VIN::iterator MAP_FLT_VIN_ITER;
 long ins[]={1,1<<1,1<<2,1<<3,1<<4,1<<5,1<<6,1<<7,1<<8,1<<9,1<<10,1<<11,1<<12,1<<13,1<<14,1<<15};
 long del[]={~1,~(1<<1),~(1<<2),~(1<<3),~(1<<4),~(1<<5),~(1<<6),~(1<<7),~(1<<8),~(1<<9),~(1<<10),~(1<<11),~(1<<12),~(1<<13),~(1<<14),~(1<<15)};
 
-void color_knodes(int* colors,int number_knodes,int cnumbers);
-PAIR_FLT_VIN search_path(int number_knodes,int* number_neighbours,int* colors,int** neighbours,float** weights,int snnumber, int* start_nodes, int plength);
+void color_nodes(int* colors,int number_nodes,int cnumbers);
+PAIR_FLT_VIN search_path(int number_nodes,int* number_neighbours,int* colors,int** neighbours,float** weights,int snnumber, int* start_nodes, int plength);
 int randint(int max);
 
 int main(int argc, char* argv[])
 {
   
-  MAP knodes;
-  MAP_ITERATOR pos;
+  MAP_STR_INT node_list1;
+  MAP_STR_INT_ITER pos;
   VEC_VEC_INT nb_vec;
   VEC_VEC_FLT we_vec;
 
-  MAP_INT_STR knode_list2;
+  MAP_INT_STR node_list2;
   MAP_INT_STR_ITER kl2_pos;
 
   FILE *datei;
@@ -91,27 +91,27 @@ int main(int argc, char* argv[])
     k1s=string(k1);
     k2s=string(k2);
 
-    if(knodes.count(k1s)==0)
+    if(node_list1.count(k1s)==0)
     {
-	knodes.insert(PAIR(k1s,i));
-	knode_list2.insert(PAIR_INT_STR(i,k1s));
+	node_list1.insert(PAIR_STR_INT(k1s,i));
+	node_list2.insert(PAIR_INT_STR(i,k1s));
 	nb_vec.push_back(VEC_INT());
 	we_vec.push_back(VEC_FLT());
 	k1i=i;
 	i++;
     } 
-    else k1i=knodes.find(k1s)->second;
+    else k1i=node_list1.find(k1s)->second;
 
-    if(knodes.count(k2s)==0)
+    if(node_list1.count(k2s)==0)
     {
-	knodes.insert(PAIR(k2s,i));
-	knode_list2.insert(PAIR_INT_STR(i,k2s));
+	node_list1.insert(PAIR_STR_INT(k2s,i));
+	node_list2.insert(PAIR_INT_STR(i,k2s));
 	nb_vec.push_back(VEC_INT());
 	we_vec.push_back(VEC_FLT());
 	k2i=i;
 	i++;
     } 
-    else k2i=knodes.find(k2s)->second;
+    else k2i=node_list1.find(k2s)->second;
 
     (nb_vec[k1i]).push_back(k2i);
     (nb_vec[k2i]).push_back(k1i);
@@ -142,18 +142,18 @@ int main(int argc, char* argv[])
   int start_nodes[start_vec.size()];
   for(j=0;j<start_vec.size();j++)
   {
-    start_nodes[j]=(knodes.find(start_vec[j]))->second;
+    start_nodes[j]=(node_list1.find(start_vec[j]))->second;
     cout<<start_vec[j]<<"  ";
   }
   
-  int number_knodes=i;
-  int number_neighbours[knodes.size()];
-  int colors[knodes.size()];
-  int* neighbours[knodes.size()];
-  float* weights[knodes.size()];
+  int number_nodes=i;
+  int number_neighbours[node_list1.size()];
+  int colors[node_list1.size()];
+  int* neighbours[node_list1.size()];
+  float* weights[node_list1.size()];
 
   cout<<endl<<endl<<"Interne Zuordnung der Knoten zu Zahlen:"<<endl;
-  for(pos=knodes.begin(),i=0;pos!=knodes.end();pos++,i++)
+  for(pos=node_list1.begin(),i=0;pos!=node_list1.end();pos++,i++)
   {
     cout<<pos->first<<"\t"<<pos->second<<"\t"<<endl;
     vec_size=nb_vec[i].size();
@@ -172,12 +172,12 @@ int main(int argc, char* argv[])
   }
   
   cout<<endl<<endl<<"Nachbarschaften:  "<<endl;
-  for(i=0;i<number_knodes;i++)
+  for(i=0;i<number_nodes;i++)
   {
-    cout<<knode_list2.find(i)->second<<" -> "<<"\t";
+    cout<<node_list2.find(i)->second<<" -> "<<"\t";
     for(j=0;j<number_neighbours[i];j++)
     {
-      cout<<knode_list2.find(neighbours[i][j])->second<<"  "<<weights[i][j]<<"\t";
+      cout<<node_list2.find(neighbours[i][j])->second<<"  "<<weights[i][j]<<"\t";
     }
     cout<<endl;
   }
@@ -190,8 +190,8 @@ int main(int argc, char* argv[])
  
   for(i=0;i<number_repeats;i++)
   {
-    color_knodes(colors,number_knodes,number_colors);
-    res=search_path(number_knodes,number_neighbours,colors,neighbours,weights,2,start_nodes,path_length);
+    color_nodes(colors,number_nodes,number_colors);
+    res=search_path(number_nodes,number_neighbours,colors,neighbours,weights,2,start_nodes,path_length);
     results.insert(res);
   }
 
@@ -201,13 +201,13 @@ int main(int argc, char* argv[])
     cout<<endl<<result_pos->first<<"\t  ";
     for(i=0;i<(result_pos->second).size();i++)
     {
-      cout<<knode_list2.find((result_pos->second)[i])->second<<" ";
+      cout<<node_list2.find((result_pos->second)[i])->second<<" ";
     }
   }
   return 0;
 }
 
-void color_knodes(int* colors,int number_knodes,int cnumber)
+void color_nodes(int* colors,int number_nodes,int cnumber)
 {
 //   long sek;
 //   int i;
@@ -215,13 +215,13 @@ void color_knodes(int* colors,int number_knodes,int cnumber)
 //   srand((unsigned) sek);
 //   cout<<rand()<<endl;
   int i;
-  for(i=0;i<number_knodes;i++)
+  for(i=0;i<number_nodes;i++)
   {
     colors[i]=rand()%cnumber;
   }
 }
 
-PAIR_FLT_VIN search_path(int number_knodes,int* number_neighbours,int* colors,int** neighbours,float** weights,int snnumber, int* start_nodes, int plength)
+PAIR_FLT_VIN search_path(int number_nodes,int* number_neighbours,int* colors,int** neighbours,float** weights,int snnumber, int* start_nodes, int plength)
 {
   MAP_PLI_PIF paths[plength];
   MAP_PLI_PIF_ITER listpos,listpos2;

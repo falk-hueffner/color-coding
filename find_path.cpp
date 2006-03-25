e9e08aba1f2cf2249cf9b2f445a1f9a81b9838d7
Falk Hueffner <hueffner@minet.uni-jena.de>
2006-03-25 23:02:25 +0000

    Improved lower bounds

--- find_path.cpp
+++ find_path.cpp
@@ -21,7 +21,12 @@ static PartialPath* find_pp(PTree& t, colorset_t c) {
 class Bounds {
 public:
     Bounds(const Problem& problem)
-	: min_neighbor_weight(problem.g.num_vertices(), WEIGHT_MAX) {
+	: min_neighbor_weight(problem.g.num_vertices(), WEIGHT_MAX),
+          min_to_goal(max_lb_edges, std::vector<weight_t>(problem.g.num_vertices(), WEIGHT_MAX)),
+          min_to_anywhere(min_to_goal),
+	  min_anywhere_to_goal(max_lb_edges, WEIGHT_MAX),
+	  min_anywhere_to_anywhere(max_lb_edges, WEIGHT_MAX),
+          min_weight(problem.path_length, problem.g.num_vertices()) {
 	std::vector<weight_t> edge_weights;
 	for (vertex_t v = 0; v < problem.g.num_vertices(); ++v)
 	    for (Graph::neighbor_it n = problem.g.neighbors_begin(v);
@@ -35,20 +40,96 @@ public:
 		 n != problem.g.neighbors_end(v); ++n)
 		if (n->weight < min_neighbor_weight[v])
 		    min_neighbor_weight[v] = n->weight;
-    }
 
-    weight_t h(vertex_t v, std::size_t edges_left) const {
-	if (edges_left > 0) {
-	    return min_neighbor_weight[v] + (edges_left - 1) * min_edge_weight;
-	} else {
-	    return edges_left * min_edge_weight;
+	for (vertex_t v = 0; v < problem.g.num_vertices(); ++v)
+	    dynprog(problem, v);
+
+	for (std::size_t l = 0; l < max_lb_edges; ++l) {
+	    min_anywhere_to_goal[l] = *std::min_element(min_to_goal[l].begin(), min_to_goal[l].end());
+	    min_anywhere_to_anywhere[l] = *std::min_element(min_to_anywhere[l].begin(),
+							    min_to_anywhere[l].end());
+	}
+
+	for (vertex_t v = 0; v < problem.g.num_vertices(); ++v) {
+	    for (std::size_t l = 0; l < problem.path_length - 1; ++l) {		
+		std::size_t edges_left = l;
+		if (edges_left) {
+		    std::size_t to_anywhere = std::min(edges_left, max_lb_edges);
+		    min_weight[l+1][v] += min_to_anywhere[to_anywhere - 1][v];
+		    edges_left -= to_anywhere;
+		}
+		while (edges_left > max_lb_edges) {
+		    min_weight[l+1][v] += min_anywhere_to_anywhere[max_lb_edges - 1];
+		    edges_left -= max_lb_edges;
+		}
+		if (edges_left)
+		    min_weight[l+1][v] += min_anywhere_to_goal[edges_left - 1];
+	    }
 	}
     }
 
+    weight_t h(vertex_t v, std::size_t edges_left) const {
+	return min_weight[edges_left][v];
+    }
+
 private:
+    void dynprog(const Problem& problem, std::size_t s) {
+	typedef std::set<vertex_t> path_t;
+	typedef std::map<path_t, weight_t> path_set_t;
+	typedef std::map<vertex_t, path_set_t> path_sets_t;
+	path_sets_t old_path_sets, new_path_sets;
+
+	path_t start_path; start_path.insert(s);
+        path_set_t start_set; start_set[start_path] = 0;
+        old_path_sets[s] = start_set;
+
+        for (std::size_t l = 0; l < max_lb_edges; ++l) {
+            new_path_sets.clear();
+	    for (path_sets_t::const_iterator psi = old_path_sets.begin();
+		 psi != old_path_sets.end(); ++psi) {
+		vertex_t v = psi->first;
+		const path_set_t& path_set = psi->second;
+		for (path_set_t::const_iterator pi = path_set.begin(); pi != path_set.end(); ++pi) {
+		    const path_t& path = pi->first;
+		    weight_t path_weight = pi->second;
+		    for (Graph::neighbor_it ni = problem.g.neighbors_begin(v);
+			 ni != problem.g.neighbors_end(v); ++ni) {
+			vertex_t w = ni->neighbor;
+			weight_t edge_weight = ni->weight;
+			path_t extended_path = path; extended_path.insert(w);
+			weight_t& extended_weight = new_path_sets[w][extended_path];
+			if (extended_weight < path_weight + edge_weight)
+			    extended_weight = path_weight + edge_weight;
+		    }
+		}
+	    }
+	    for (path_sets_t::const_iterator psi = new_path_sets.begin();
+		 psi != new_path_sets.end(); ++psi) {
+		vertex_t v = psi->first;
+		const path_set_t& path_set = psi->second;
+		for (path_set_t::const_iterator pi = path_set.begin(); pi != path_set.end(); ++pi) {
+		    weight_t path_weight = pi->second;
+		    if (path_weight < min_to_anywhere[l][s])
+			min_to_anywhere[l][s] = path_weight;
+		    if (problem.is_end_vertex[v])
+			if (path_weight < min_to_goal[l][s])
+			    min_to_goal[l][s] = path_weight;
+		}
+	    }
+	    std::swap(old_path_sets, new_path_sets);
+        }
+    }
+
+    static const std::size_t max_lb_edges = 3;
     weight_t min_edge_weight;
     std::vector<weight_t> min_neighbor_weight;
+    std::vector<std::vector<weight_t> > min_to_goal;
+    std::vector<std::vector<weight_t> > min_to_anywhere;
+    std::vector<weight_t> min_anywhere_to_goal;
+    std::vector<weight_t> min_anywhere_to_anywhere;
+    std::vector<std::vector<weight_t> > min_weight;
 };
+const std::size_t Bounds::max_lb_edges;
 
 bool dynprog_trial(const ColoredGraph& g,
 		   const std::vector<vertex_t>& start_vertices,

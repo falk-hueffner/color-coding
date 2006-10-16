0691008c315824041ecafd326819544d38ec90fd
Falk Hueffner <hueffner@minet.uni-jena.de>
2006-10-16 11:38:02 +0000

    Use stdint.h.

--- types.h
+++ types.h
@@ -3,21 +3,17 @@
 
 #include <float.h>
 #include <limits.h>
+#include <stdint.h>
 
 #include "util.h"
 
-typedef unsigned short my_uint16_t;
-typedef unsigned int   my_uint32_t;
-
 COMPILE_TIME_ASSERT(CHAR_BIT == 8);
-COMPILE_TIME_ASSERT(sizeof (my_uint16_t) == 2);
-COMPILE_TIME_ASSERT(sizeof (my_uint32_t) == 4);
 
 typedef unsigned     vertex_t;
-typedef my_uint16_t  small_vertex_t;
+typedef uint16_t     small_vertex_t;
 typedef float        weight_t;
 typedef unsigned     color_t;
-typedef my_uint32_t  colorset_t;
+typedef uint32_t     colorset_t;
 
 #define MAX_COLORS (sizeof (colorset_t) * CHAR_BIT - 1) // one bit is used up in PTree::Node
 #define MAX_VERTEX ((small_vertex_t) -1)
